0e4524d7fdfe2b953840d393822379ecebb06032
unikal1 <jack8226123@gmail.com>
2026-01-15 09:49:59 +0900

    init commit

---
+++ src/test/java/com/plog/PlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
