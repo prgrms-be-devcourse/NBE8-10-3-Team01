76b5683de96368efc9175703b05e236f1696f6c0
Minhee Han <yyj9694651@gmail.com>
2026-01-19 12:38:18 +0900

    Feat: 사용자 정의 인증 객체 SecurityUser 구현
    - Spring Security의 User 클래스를 상속
    - 고유 식별자(id), email, nickname 필드를 추가
    - getUsername()이 이메일을 반환하도록 부모 클래스 생성자 연동 및 getEmail() 메서드 추가

---
+++ src/main/java/com/plog/global/security/SecurityUser.java
@@ -0,0 +1,58 @@
+package com.plog.global.security;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+/**
+ * Spring Security의 인증 인터페이스를 구현한 사용자 정의 인증 객체입니다.
+ * <p>
+ * 시스템 내에서 인증된 사용자의 정보를 세션이나 SecurityContext에 보관하며,
+ * 사용자의 식별자(id), 이메일, 닉네임 등의 기본 정보를 제공합니다.
+ *
+ * <p><b>상속 정보:</b><br>
+ * {@link User} 클래스를 상속받아 구현되었습니다.
+ *
+ * <p><b>주요 생성자:</b><br>
+ * {@code @Builder SecurityUser(Long id, String email, String password, String nickname, Collection authorities)}<br>
+ * 빌더 패턴을 통해 생성되며, 부모 클래스인 User에 인증 정보를 전달합니다.
+ *
+ * <p><b>빈 관리:</b><br>
+ * UserDetailsService 구현체에서 사용자 정보를 조회하여 인스턴스를 생성합니다.
+ *
+ * <p><b>외부 모듈:</b><br>
+ * Spring Security Core 모듈을 활용합니다.
+ *
+ * @author yyj96
+ * @see org.springframework.security.core.userdetails.UserDetails
+ * @since 2026-01-16
+ */
+
+@Getter
+public class SecurityUser extends User {
+    private final Long id;
+    private final String email;
+    private final String nickname;
+
+    @Builder
+    public SecurityUser(
+            Long id,
+            String email,
+            String password,
+            String nickname,
+            Collection<? extends GrantedAuthority> authorities
+    ) {
+        super(email, password, authorities);
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return super.getUsername();
+    }
+}
