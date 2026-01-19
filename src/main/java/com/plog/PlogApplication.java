19959b9e6667ccbd0ecc1d221c29e1a47440f20f
onethousand121 <onethousand121@gmail.com>
2026-01-19 10:01:23 +0900

    MinIO 설정 및 의존성 추가

--- src/main/java/com/plog/PlogApplication.java
+++ src/main/java/com/plog/PlogApplication.java
@@ -2,6 +2,8 @@ package com.plog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication
 public class PlogApplication {
