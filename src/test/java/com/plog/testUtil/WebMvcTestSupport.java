0026a225d9070e0fa5b4165775f9ad6b0294ef7c
Minhee Han <yyj9694651@gmail.com>
2026-02-27 17:10:02 +0900

    Feat: 테스트 컨텍스트에 시큐리트 필터를 제외하도록 설정 및 CommentControllerTest도 WebMvcTestSupport를 상속하도록 수정

--- src/test/java/com/plog/testUtil/WebMvcTestSupport.java
+++ src/test/java/com/plog/testUtil/WebMvcTestSupport.java
@@ -3,13 +3,24 @@ package com.plog.testUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.kotlin.KotlinModule;
 import com.plog.domain.member.service.AuthService;
+import com.plog.global.auth.CustomOAuth2UserService;
+import com.plog.global.auth.oauth2.handler.OAuth2SuccessHandler;
 import com.plog.global.security.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.security.oauth2.client.autoconfigure.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.security.oauth2.client.autoconfigure.servlet.OAuth2ClientWebSecurityAutoConfiguration;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * WebMvcTest 에 대한 테스트 유틸입니다.
@@ -24,10 +35,29 @@ import org.springframework.test.web.servlet.MockMvc;
  * @author jack8
  * @since 2026-01-20
  */
+
+@WebMvcTest
+@EnableAutoConfiguration(exclude = {
+        SecurityAutoConfiguration.class,
+        SecurityFilterAutoConfiguration.class,
+        OAuth2ClientAutoConfiguration.class,
+        OAuth2ClientWebSecurityAutoConfiguration.class
+})
+@AutoConfigureMockMvc(addFilters = false)
+@Import({SecurityTestConfig.class})
 public abstract class WebMvcTestSupport {
 
     @Autowired
+    protected WebApplicationContext context;
+
     protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUpMockMvc() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
 
     protected final ObjectMapper objectMapper = new ObjectMapper()
             .registerModule(new KotlinModule.Builder().build())
@@ -50,4 +80,13 @@ public abstract class WebMvcTestSupport {
 
     @MockitoBean
     protected CustomUserDetailsService customUserDetailsService;
+
+    @MockitoBean
+    protected CustomOAuth2UserService customOAuth2UserService;
+
+    @MockitoBean
+    protected OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @MockitoBean
+    protected CustomAuthenticationFilter customAuthenticationFilter;
 }
